89a2e6602a7ba03b703beacbe51f5d3033913f49
Martin <martin_lenart@icloud.com>
2024-04-17 09:02:32 +0200

    update

--- MyApps/02-app/src/App.js
+++ MyApps/02-app/src/App.js
@@ -20,3 +20,14 @@ function App() {
 }
 
 export default App;
+
+/* Exercises
+
+1. Study the component ReadWebApiFunc06 in M4L1, 05-lifecycle, 06-using-lifecycle-async.js
+   - implement in component Home, pages, home.js communication with the WebApi and fill in the details of 
+    number of groups, number of artists, number of albums
+    
+2. Add a page to the application that uses the component, AnimalInModal11a, in M4L3, 11a-form-animal-in-modal.js
+   - the page should create a list of animals, by allowing the user to add one animal at the time from a modal form.
+
+*/
